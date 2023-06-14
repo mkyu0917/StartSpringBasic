696771a01fbf997bf274945aaf7cd22a5669c1d5
mkyu0917 <mkyu0917@gmail.com>
2023-06-14 22:50:03 +0900

    일단올려봐

---
+++ src/test/java/com/spring/springbootstarter/SpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
