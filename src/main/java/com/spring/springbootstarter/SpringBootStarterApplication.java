6ac988d7225a42872edc001415a3294a329c2874
mkyu0917 <mkyu0917@gmail.com>
2023-06-15 21:05:19 +0900

    화면테스트 커밋

--- src/main/java/com/spring/springbootstarter/SpringBootStarterApplication.java
+++ src/main/java/com/spring/springbootstarter/SpringBootStarterApplication.java
@@ -9,7 +9,7 @@ public class SpringBootStarterApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootStarterApplication.class, args);
 
-        System.out.println("");
+
 
     }
 
