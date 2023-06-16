f6cc13d28169b7734d03e2b1be3e908cf4d599f4
mkyu0917 <mkyu0917@gmail.com>
2023-06-17 00:24:52 +0900

    controller, view 예제

--- src/main/java/com/spring/springbootstarter/controller/HelloController.java
+++ src/main/java/com/spring/springbootstarter/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.spring.springbootstarter.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -15,4 +16,9 @@ public class HelloController {
         return "hello";
     }
 
+    @GetMapping("/start-mvc")
+    public String startMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "start-mvc";
+    }
 }
